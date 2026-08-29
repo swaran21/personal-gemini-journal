a71566c9afead677ccae28e208b02a30836a2329
swaran21 <saiswaran212005@gmail.com>
2026-08-29 14:20:03 +0530

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/pm/personalgeminijournalbackend/PersonalGeminiJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pm.personalgeminijournalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalGeminiJournalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalGeminiJournalBackendApplication.class, args);
+    }
+
+}
